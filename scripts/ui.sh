fa4ac8047e9d65d687c5d6fa9842e9aa77f377e8
Craig Drummond <craig.p.drummond@gmail.com>
2020-08-10 16:54:41 +0100

    Update JDK path

--- scripts/ui.sh
+++ scripts/ui.sh
@@ -1,4 +1,4 @@
-JAVA_HOME=/usr/lib/jvm/java-14-openjdk-14.0.1.7-2.rolling.fc32.i386
+JAVA_HOME=/usr/lib/jvm/java-14-openjdk-14.0.2.12-1.rolling.fc32.i386
 PATH=$PATH:$JAVA_HOME/bin
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $DIR/../MusicMagicMixer
